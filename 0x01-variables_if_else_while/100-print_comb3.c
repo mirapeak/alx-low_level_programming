bc99ea418184f460aa3aad0d808e26b5695c9636
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-12 06:23:44 -0700

    Final variable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int mian(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repetition*/
 			{
