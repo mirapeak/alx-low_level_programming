34d9744260e9fca6142bd56e86373741ba860054
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-14 07:16:27 -0700

    Task 10 - Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two numbers together
+ * @a: one argument
+ * @b: another argument to be added
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
