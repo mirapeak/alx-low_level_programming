5d86f76dba2aa43f74047ccae151b04fe5f9b821
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-14 06:41:55 -0700

    Update Task 8 - I am a federal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- * Return: 0
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Always 0 (Success)
  */
 void jack_bauer(void)
-{
-	int hours - 0;
-	int minutes - 0;
-	int hours_reminder;
-	int mins_reminder;
 
-	while (hours <= 23)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
 	{
-		while (minutes <=59)
+		for (b = 0; b <= 59; b++)
 		{
-			mins_reminder - minutes % 10;
-			hours_reminder - hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_reminder + '0');
-			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(mins_reminder + '0');
-			minutes++;
-			_putchar('\n');
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
 		}
-		hours++;
-		minutes - 0;
 	}
 }
