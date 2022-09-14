2145b87e6a234ae00c545050c3634b1672915b3b
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-14 08:00:35 -0700

    Task 101 - natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 1;
+	int total = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
+		i++;
+	}
+	printf("%d\n", total);
+	return (0)'
+}
