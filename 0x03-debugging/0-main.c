4d82c403aca9cfebbe0634e408b645b736e31691
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-15 04:57:17 -0700

    Task 0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print positive or negative integer
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
