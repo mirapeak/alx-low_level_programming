8903256d4ac016e1fb22d102783f53ab13849168
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-15 20:31:25 -0700

    Task 4 - I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all integers except 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	putchar(10);
+}
