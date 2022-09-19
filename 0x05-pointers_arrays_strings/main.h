9991579f653f232e2d08098f9aa9601cf35406e6
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-19 16:47:14 -0700

    Task 0. 98 Battery st -  final update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,17 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a; int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a; int n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void rev_string(char *s);
 int _atoi(char *s);
 
 #endif
