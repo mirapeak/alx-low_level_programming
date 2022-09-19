bed979b31d2c052ef272447aa848eb78980456f8
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-19 01:41:53 -0700

    Task 0 - updated 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * reset_to_98 -  function that takes a pointer to an int as parameter
  * @n: Entry point
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
