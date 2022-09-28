1a0c1f44e4364b3859075af8132f672393b73170
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-28 07:06:23 -0700

    Last update to Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,46 +1,40 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string parameter
- * Return: recursion
+ * _strlen_recursion - function that return the length of a string
+ * @s: pointer to string
+ * Return: length of s
  */
 int _strlen_recursion(char *s)
 {
-	if (!*s)
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
-
 /**
- * p1 - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolens
+ * palcheck - checker
+ * @x: iterration
+ * @y: length of s
+ * @s: string
+ * Return: 1 or 0
  */
-int p1(char *s, int l)
+int palcheck(int x, int y, char *s)
 {
-	if (l < 1)
-	{
+	if (x >= y)
 		return (1);
-	}
-	if (*s == *(s + 1))
-	{
-		return (p1(s + 1, l - 2));
-	}
-	return (0);
+	if (*(s + x) != *(s + y - x))
+		return (0);
+	return (palcheck(x + 1, y, s));
 }
-
 /**
- * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
+ * is_palindrome - check if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome otherwise 0
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	int i = _strlen_recursion(s);
 
-	return (p1(s, len - 1));
+	return (palcheck(0, i - 1, s));
 }
