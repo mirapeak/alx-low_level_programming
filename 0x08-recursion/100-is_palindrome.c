61974f666f70cd37badd125aadec13fe5384955e
mirapeak <livinusmiracle2009@yahoo.com>
2022-09-28 06:55:30 -0700

    Update to Task 7 - advance task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,46 @@
 #include "main.h"
 
 /**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not
- * @s: string to be checked
- * Return: 1 if s is a palindrome, otherwise 0
- */
-int is_palindrome(char *s)
-{
-	int flag = 1;
-
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
-
-/**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * @flag: flag to indicate if string is a palindrome
- * Return: void
- */
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start - 1, end - 1, flag);
-	}
-}
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- * Return: length of the string
+ * _strlen_recursion - size
+ * @s: pointer to string parameter
+ * Return: recursion
  */
 int _strlen_recursion(char *s)
 {
-	int sum = 0;
-
-	if (*s != '\0')
+	if (!*s)
 	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
+		return (0);
 	}
-	return (sum);
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolens
+ */
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + 1))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
