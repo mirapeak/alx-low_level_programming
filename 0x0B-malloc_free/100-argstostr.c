5cb8dd8a97881953f96f9f5f75bc5a71ebdef050
mirapeak <livinusmiracle2009@yahoo.com>
2022-10-04 07:32:20 -0700

    Updated task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,6 +5,7 @@
  * *argstostr - concatenates all the arguments of the program
  * @ac: number of arguments
  * @av: array of arguments
+ *
  * Return: Pointer to the new string (Success), NULL (Error)
  */
 char *argstostr(int ac, char **av)
@@ -39,6 +40,5 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
-
 	return (str);
 }
