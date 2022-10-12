c2f1f996a6db640302497238e312051a2098225c
mirapeak <livinusmiracle2009@yahoo.com>
2022-10-12 07:17:01 -0700

    Task 2 - I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,30 +12,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, res;
+	int i;
 
-	res = -1;
 	if (array && cmp)
 	{
-
-		if (size <= 0)
-		{
-			return (res);
-		}
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
-			if (cmp(array[i]) > 0)
-			{
-				res = 1;
-				break;
-			}
-			if ((cmp(array[i]) == (-1)))
-			{
-				return (res);
-			}
+			if (cmp(array[i]) != 0)
+				return (1);
 		}
-
 	}
-	return (res);
+
+	return (-1);
 }
